b74e4c45bec3018492c7236ecce3a79c74a2726f
mhmoma <mhmoma667@gmail.com>
2025-12-02 20:58:17 +0800

    feat: Project update on 2025/12/02 周二 20:58:17.03

--- upload_to_git.bat
+++ upload_to_git.bat
@@ -3,8 +3,9 @@ set REPO_URL=https://github.com/mhmoma/-.git
 
 REM --- Git 操作 ---
 echo.
-echo Initializing Git repository...
+echo Initializing Git repository and setting main branch...
 git init
+git checkout -b main
 
 echo.
 echo Adding all files to staging...
@@ -16,8 +17,7 @@ git commit -m "feat: Project update on %date% %time%"
 
 echo.
 echo Setting remote repository...
-git remote rm origin
-git remote add origin %REPO_URL%
+git remote add origin %REPO_URL% 2>nul || git remote set-url origin %REPO_URL%
 
 echo.
 echo Pushing to the main branch...
